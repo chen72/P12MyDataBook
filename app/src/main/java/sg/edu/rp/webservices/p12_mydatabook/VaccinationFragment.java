20f224e10197b3e6541e18652e5ecb482da7e891
chen72 <16046469@myrp.edu.sg>
2018-08-06 15:37:36 +0800

    sharepreferences

--- app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/VaccinationFragment.java
+++ app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/VaccinationFragment.java
@@ -1,17 +1,36 @@
 package sg.edu.rp.webservices.p12_mydatabook;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class VaccinationFragment extends Fragment {
+
+    View view;
+    Button btnEdit;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    SharedPreferences prefs;
+    TextView tv;
 
 
     public VaccinationFragment() {
@@ -23,7 +42,61 @@ public class VaccinationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.vaccinationfragment, container, false);
+        view = inflater.inflate(R.layout.vaccinationfragment, container, false);
+        btnEdit = (Button) view.findViewById(R.id.btnEdit);
+
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String name = prefs.getString("vacc", "");
+
+        tv = (TextView) view.findViewById(R.id.bioTV);
+        tv.setText(name);
+
+
+        drawerLayout = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+        drawerList = (ListView) getActivity().findViewById(R.id.left_drawer);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //action
+                drawerLayout.openDrawer(drawerList);
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                LayoutInflater inflater = (LayoutInflater)
+                        getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LinearLayout passPhrase =
+                        (LinearLayout) inflater.inflate(R.layout.dialogactivity, null);
+                final EditText etSomething = (EditText) passPhrase
+                        .findViewById(R.id.etBio);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setTitle("Edit bio")
+                        .setView(passPhrase)
+                        .setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                                SharedPreferences.Editor prefEdit = prefs.edit();
+                                prefEdit.putString("vacc", etSomething.getText().toString().trim());
+                                prefEdit.commit();
+                                tv.setText(etSomething.getText().toString().trim());
+                            }
+                        }).setNegativeButton("Cancel", null);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+            }
+        });
+
+
+        return view;
     }
 
 }
